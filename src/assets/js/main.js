f9fea94a072a5b3246001abaf4fad590240134a6
Marcos Betancor <86015926+Chafa23@users.noreply.github.com>
2025-02-04 02:24:44 -0300

    Update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -194,17 +194,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 function openContactForm(product) {
     document.querySelector('#message').value = `Consulta sobre: ${product}`;
     document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
